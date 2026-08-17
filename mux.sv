deb732f4933d25e4e10caf92d7ee73fe99f6c11e
saviorikelme <savio.rikelme@gmail.com>
2026-08-17 15:00:18 -0300

    Update mux.sv

--- mux.sv
+++ mux.sv
@@ -5,22 +5,21 @@ module mux
     input  logic [1:0]  sel
   ); 
 
-  // Fios internos para guardar as saídas das portas AND e os seletores invertidos
+  // fios internos para guardar as saídas das portas and e os seletores invertidos
   logic [31:0] f1, f2, f3, f4;
   logic n_sel1, n_sel0;
 
-  // Inversoras para os 2 bits do seletor (criando sel negado)
+  // Inversoras para os 2 bits do seletor
   not g1(n_sel0, sel[0]),
       g2(n_sel1, sel[1]);
 
-  // Portas AND replicadas 32 vezes (Array de Instâncias)[cite: 1]
-  // Cada instância pega 1 bit dos dados (a, b, c, d) e os mesmos sinais escalares do seletor
+  // fios internos guardando as informacoes dada a chave de selecao
   and g3 [31:0] (f1, a, n_sel1, n_sel0),
       g4 [31:0] (f2, b, n_sel1, sel[0]),
       g5 [31:0] (f3, c, sel[1], n_sel0),
       g6 [31:0] (f4, d, sel[1], sel[0]);
 
-  // Porta OR replicada 32 vezes para juntar tudo[cite: 1]
+  // porta or para saida final
   or  g7 [31:0] (f, f1, f2, f3, f4);
 
 endmodule
